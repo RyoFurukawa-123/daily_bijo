e073169f26e7639840165fa72e3c2a33a44f7602
Ryo Furukawa <60736174+RyoFurukawa-123@users.noreply.github.com>
2020-03-11 15:57:52 +0900

    collection.erb,main,

--- app.rb
+++ app.rb
@@ -1,7 +1,17 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'sinatra/cookies'
+
 enable :sessions
+
+def db
+    PG::connect(
+    host: 'localhost', #今回はローカル環境なのでlocalhost
+    user: 'furukawaryou', #ターミナルでwhoamiを実行し表示されたユーザー名
+    password: '', #接続時のパスワード今回は設定しないので、空白
+    dbname: 'bijoapp' #作成したデータベース名
+    ) 
+end
 
 get "/" do
     @title = "DailyBijo"
@@ -16,12 +26,18 @@ end
 
 post "/signin" do
     session[:user] = params[:name]
-    redirect '/mypage'
+    redirect '/main'
 end
 
-# マイページ
-get "/mypage" do
+# メイン
+get "/main" do
     @name = session[:user]
+    return erb :main
+end
+
+#マイページ
+get "/mypage" do
+
     return erb :mypage
 end
 
@@ -29,4 +45,9 @@ end
 post "/mypage" do 
     session[:user] = nil
     redirect '/signin'
+end
+
+#コレクション
+get "/collection" do
+    return erb :collection
 end
